4a1297fcd3826d45b028a9d01672d924dd41fa22
jivak1 <jivak1@abv.bg>
2024-03-11 16:45:50 +0200

    code refactory

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import jdk.jshell.JShell;
+
 import java.util.*;
 
 public class Main {
+    //checks if trips with the current sal weight are possible
     public static boolean canDoTrips(int n, int k, List<Integer> sheep, int weight){
 
         Queue<Integer> sheepQueue = new ArrayDeque<>(sheep) ;
@@ -34,6 +37,7 @@ public class Main {
             return false ;
         }
     }
+    //checks what is the minimum sal weight starting from arithmetic mean
     public static int checkMinimumWeight(int n, int k, List<Integer> sheep, int salMinimumWeightGuess){
         for (int i = salMinimumWeightGuess;  ; i++){
             if(canDoTrips(n, k, sheep, i)){
@@ -41,34 +45,52 @@ public class Main {
             }
         }
     }
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in) ;
-
-        int n = scanner.nextInt() ;
-        int k = scanner.nextInt();
-
-        List<Integer> sheep = new ArrayList<>() ;
-        //input
-        for (int i = 0; i < n; i++){
-            sheep.add(scanner.nextInt()) ;
-        }
-        /////
-        //sort array
-        sheep.sort((o1, o2) -> {
-            return o2 - o1;
-        });
-        //find sum of weights
+    public static int findWeightSum(List<Integer> sheep, int n){
         int weightsSum = 0 ;
 
         for(int i = 0; i < sheep.size(); i++){
             weightsSum += sheep.get(i) ;
         }
+
+        return weightsSum ;
+    }
+    public static void input(List<Integer> sheep, Scanner scanner, int n){
+        for (int i = 0; i < n; i++){
+            sheep.add(scanner.nextInt()) ;
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in) ;
+
+        List<Integer> sheep = new ArrayList<>() ;
+
+        int n = scanner.nextInt() ;
+        int k = scanner.nextInt();
+
+
+        //input
+        input(sheep, scanner, n) ;
+        /////
+
+        //sort array
+        sheep.sort((o1, o2) -> {
+            return o2 - o1;
+        });
+
+        //find sum of weights
+        int weightsSum = findWeightSum(sheep, n) ;
         //
-        int salMinimumWeightGuess = (int)Math.ceil(weightsSum/k) ;
 
+        //find arithmetic mean of sheep weights per trip
+        int salMinimumWeightGuess = (int)Math.floor(weightsSum/k) ;
+        //
+
+        //find sal minimum wight
         int salMinimumWeight = checkMinimumWeight(n, k, sheep, salMinimumWeightGuess) ;
+        //
 
+        //print minimum weight
         System.out.println(salMinimumWeight);
     }
 }
